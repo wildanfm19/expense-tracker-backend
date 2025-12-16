c7cd5d67b28052d908d47238cdda1935a8661f65
wildanfm19 <dracko.maaruftm@gmail.com>
2025-12-16 12:33:04 +0700

    Change enum to string in the database migration

--- database/migrations/2025_12_10_174901_create_transactions_table.php
+++ database/migrations/2025_12_10_174901_create_transactions_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
         Schema:: create('transactions', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
-            $table->enum('type', ['income', 'expense']);
+            $table->string('type'); 
             $table->decimal('amount', 15, 2);
             $table->string('category');
             $table->text('description')->nullable();
